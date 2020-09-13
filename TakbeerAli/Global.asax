f7eb9c0ef9c8290bf8ccdd84b3be750740d5fa6f
TakbeerAli <Takbeeralikhan@gmail.com>
2020-09-13 21:36:53 +0500

    first commit

---
+++ TakbeerAli/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TakbeerAli.MvcApplication" Language="C#" %>
---
+++ TakbeerAli/Models/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TakbeerAli.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+
+        public string Capital { get; set; }
+    }
+
+    public class CountryDbContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+    }
+}
